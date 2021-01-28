ce07a187650ba11e09429fd9352538b1284d9c81
GitHub Action <action@github.com>
2021-01-28 12:24:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/credentivesec/intellijlxdinterpreter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.credentivesec.intellijlxdinterpreter.services
+
+import com.github.credentivesec.intellijlxdinterpreter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/credentivesec/intellijlxdinterpreter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/credentivesec/intellijlxdinterpreter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.credentivesec.intellijlxdinterpreter.services
 
+import com.github.credentivesec.intellijlxdinterpreter.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
